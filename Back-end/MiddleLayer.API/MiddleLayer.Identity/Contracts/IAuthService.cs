f8b60964297f3919b62732ad2414ab025f26eb30
TSDilov <cv.dilov@abv.bg>
2024-03-16 21:37:34 +0200

    Creating project for Identity and making the models and IAuthContract

---
+++ Back-end/MiddleLayer.API/MiddleLayer.Identity/Contracts/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using TaskManager.Application.Models;
+
+namespace TaskManager.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> Login(AuthRequest request);
+
+        Task<RegistrationResponse> Register(RegistrationRequest request);
+
+        Task<AuthResponse> RegisterUserFromGoogleAsync(string email);
+
+        Task<AuthResponse> GetUserByEmailAsync(string email);
+    }
+}
---
+++ Back-end/MiddleLayer.API/MiddleLayer.Identity/Models/RegistrationResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace MiddleLayer.Identity.Models
+{
+    public class RegistrationResponse
+    {
+        public string UserId { get; set; }
+    }
+}
