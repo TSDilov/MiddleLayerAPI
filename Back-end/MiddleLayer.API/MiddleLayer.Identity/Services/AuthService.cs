78ef61e90d039b1bbd84f33ab454ce635f353951
TSDilov <cv.dilov@abv.bg>
2024-03-18 12:04:42 +0200

    Injecting logger for the AuthService

--- Back-end/MiddleLayer.API/MiddleLayer.Identity/Services/AuthService.cs
+++ Back-end/MiddleLayer.API/MiddleLayer.Identity/Services/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using MiddleLayer.Identity.Contracts;
@@ -14,77 +15,97 @@ namespace MiddleLayer.Identity.Services
         private readonly UserManager<ApplicationUser> userManager;
         private readonly IOptionsMonitor<JwtSettings> jwtSettings;
         private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly ILogger<AuthService> logger;
 
         public AuthService(UserManager<ApplicationUser> userManager,
             IOptionsMonitor<JwtSettings> jwtSettings,
-            SignInManager<ApplicationUser> signInManager)
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<AuthService> logger)
         {
             this.userManager = userManager;
             this.jwtSettings = jwtSettings;
             this.signInManager = signInManager;
+            this.logger = logger;
         }
 
         public async Task<AuthResponse> Login(AuthRequest request)
         {
-            var user = await this.userManager.FindByEmailAsync(request.Email);
-            if (user == null)
+            try
             {
-                throw new Exception($"User with {request.Email} not found.");
+                var user = await this.userManager.FindByEmailAsync(request.Email);
+                if (user == null)
+                {
+                    throw new Exception($"User with {request.Email} not found.");
+                }
+
+                var result = await this.signInManager.PasswordSignInAsync(user.UserName, request.Password, false, lockoutOnFailure: false);
+                if (!result.Succeeded)
+                {
+                    throw new Exception($"Credentials for {request.Email} aren't valid");
+                }
+
+                JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
+                var response = new AuthResponse
+                {
+                    Id = user.Id,
+                    Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                    Email = user.Email,
+                    UserName = user.UserName,
+                };
+
+                return response;
             }
-
-            var result = await this.signInManager.PasswordSignInAsync(user.UserName, request.Password, false, lockoutOnFailure: false);
-            if (!result.Succeeded)
+            catch (Exception ex)
             {
-                throw new Exception($"Credentials for {request.Email} aren't valid");
+                this.logger.LogError(ex, "An error occurred during login.");
+                throw;
             }
-
-            JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
-            var response = new AuthResponse
-            {
-                Id = user.Id,
-                Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
-                Email = user.Email,
-                UserName = user.UserName,
-            };
-
-            return response;
         }
 
         public async Task<RegistrationResponse> Register(RegistrationRequest request)
         {
-            var existingUser = await this.userManager.FindByNameAsync(request.UserName);
-            if (existingUser != null)
+            try
             {
-                throw new Exception($"Username {request.UserName} already exists");
-            }
-
-            var user = new ApplicationUser
-            {
-                Email = request.Email,
-                FirstName = request.FirstName,
-                LastName = request.LastName,
-                UserName = request.UserName,
-                EmailConfirmed = true
-            };
-
-            var existingEmail = await this.userManager.FindByEmailAsync(request.Email);
-            if (existingEmail == null)
-            {
-                var result = await this.userManager.CreateAsync(user, request.Password);
-                if (result.Succeeded)
+                var existingUser = await this.userManager.FindByNameAsync(request.UserName);
+                if (existingUser != null)
                 {
-                    await this.userManager.AddToRoleAsync(user, RoleConstants.User);
-                    return new RegistrationResponse() { UserId = user.Id };
+                    throw new Exception($"Username {request.UserName} already exists");
+                }
+
+                var user = new ApplicationUser
+                {
+                    Email = request.Email,
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                    UserName = request.UserName,
+                    EmailConfirmed = true
+                };
+
+                var existingEmail = await this.userManager.FindByEmailAsync(request.Email);
+                if (existingEmail == null)
+                {
+                    var result = await this.userManager.CreateAsync(user, request.Password);
+                    if (result.Succeeded)
+                    {
+                        await this.userManager.AddToRoleAsync(user, RoleConstants.User);
+                        return new RegistrationResponse() { UserId = user.Id };
+                    }
+                    else
+                    {
+                        throw new Exception($"{string.Join(",", result.Errors.Select(x => x.Description))}");
+                    }
                 }
                 else
                 {
-                    throw new Exception($"{string.Join(",", result.Errors.Select(x => x.Description))}");
+                    throw new Exception($"Email {request.Email} already exists.");
                 }
             }
-            else
+            catch (Exception ex)
             {
-                throw new Exception($"Email {request.Email} already exists.");
+                this.logger.LogError(ex, "An error occurred during registration.");
+                throw;
             }
+            
         }
 
         private async Task<JwtSecurityToken> GenerateToken(ApplicationUser user)
